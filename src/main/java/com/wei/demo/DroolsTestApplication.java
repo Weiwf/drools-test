007e234b04f137f09fb8e269099d6c910ee196a6
韦阿丰 <790107894@qq.com>
2018-11-20 18:09:24 +0800

    'init'

---
+++ src/main/java/com/wei/demo/DroolsTestApplication.java
@@ -0,0 +1,20 @@
+package com.wei.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+
+@SpringBootApplication
+public class DroolsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsTestApplication.class, args);
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		return KieServices.Factory.get().getKieClasspathContainer();
+	}
+}
